6bef96e28a5661fc078149ef9ee05aadafde2211
Eric Tam <eric.tam@traceyanderic.com>
2017-10-22 20:52:34 +1100

    moved away from ATDIS and use horizon_xml gem to fetch data

--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,15 @@
-#!/usr/bin/env ruby
-Bundler.require
+require 'scraperwiki'
+require 'horizon_xml'
 
-ATDISPlanningAlertsFeed.save('http://myhorizon.solorient.com.au/Horizon/walcha/atdis/1.0')
+collector = Horizon_xml.new
+collector.setPeriod(ENV['MORPH_PERIOD']).setDomain('horizondap_walcha').setInfoUrl(collector.host_url).setCommentUrl('mailto:council@walcha.nsw.gov.au')
+
+collector.getRecords.each do |record|
+#   p record
+  if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+    puts "Saving record " + record['council_reference'] + ", " + record['address']
+    ScraperWiki.save_sqlite(['council_reference'], record)
+  else
+    puts "Skipping already saved record " + record['council_reference']
+  end
+end
