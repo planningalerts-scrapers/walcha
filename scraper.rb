ba75dbc3e99b5b1bf8f844e85a8d78d3c5c4b4bc
Eric Tam <eric.tam@traceyanderic.com>
2017-10-23 14:12:54 +1100

    updated to use horizon_xml gem v1.1

--- scraper.rb
+++ scraper.rb
@@ -2,7 +2,10 @@ require 'scraperwiki'
 require 'horizon_xml'
 
 collector = Horizon_xml.new
-collector.setPeriod(ENV['MORPH_PERIOD']).setDomain('horizondap_walcha').setInfoUrl(collector.host_url).setCommentUrl('mailto:council@walcha.nsw.gov.au')
+collector.base_url    = 'http://myhorizon.solorient.com.au/Horizon/'
+collector.domain      = 'horizondap_walcha'
+collector.comment_url = 'mailto:council@walcha.nsw.gov.au'
+collector.period      = ENV['MORPH_PERIOD']
 
 collector.getRecords.each do |record|
 #   p record
